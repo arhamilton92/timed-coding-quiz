52d82f8397400fc546fe71767092e8cf472ec443
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-11 19:07:37 -0400

    added object containing questions, answers and correct answers.

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,33 @@ var startBtn = document.getElementById("start-button");
 var timeLeft = document.getElementById("time");
 var secondsLeft = "60";
 
-
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answersArray: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer: "3. alerts",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ___.",
+        answersArray: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "3. parentheses",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        answersArray: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "4. all of the above",
+    }
+    {
+        question: "String values must be enclosed within ___ whwn being assigned to variables.",
+        answersArray: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "3. quotes",
+    }
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answersArray: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "4. console.log",
+    }
+]
 
 //function 
 function startTimer() {
